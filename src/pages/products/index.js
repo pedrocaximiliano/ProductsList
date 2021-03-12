7a6aaceb0346173a8d97e49998d37efc0e2e4efa
Pedro Caximiliano <pedro.neto@ab-inbev.com>
2021-03-12 18:50:05 -0300

    comite teste

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,33 +1,32 @@
-import React, { Component } from 'react';
-import api from '../../services/api';
-import './styles.css';
+import React, { Component } from "react";
+import api from "../../services/api";
+import "./styles.css";
 
-class products extends Component{
-    state = {
-        productsInfo: [],
-    };
+class products extends Component {
+  state = {
+    productsInfo: [],
+  };
 
-    async componentDidMount() {
-        const {id} =this.props.match.params;
-        const response = await api.get(`products/${id}`);
-        this.setState({
-            productsInfo: response.data,
-        })
-        console.log(response);
-
-    }
-  render() {
-      const { productsInfo} = this.state;
-      return (
-          <div className="product-info">
-          <h1>{productsInfo.title}</h1>
-            <p>{productsInfo.description}</p>
-            <p>
-                URL: <a href={productsInfo.url}>{productsInfo.url}</a>
-            </p>
-          </div>
-      )
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+    const response = await api.get(`products/${id}`);
+    this.setState({
+      productsInfo: response.data,
+    });
+    console.log(response);
   }
-};
+  render() {
+    const { productsInfo } = this.state;
+    return (
+      <div className="product-info">
+        <h1>{productsInfo.title}</h1>
+        <p>{productsInfo.description}</p>
+        <p>
+          LINK: <a href={productsInfo.url}>{productsInfo.url}</a>
+        </p>
+      </div>
+    );
+  }
+}
 
 export default products;
